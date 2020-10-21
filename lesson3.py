f70e57f01c6e9ee1e173b2a54434ff038f3a4e5c
zil93rus <Zelovanastya2011>
2020-10-21 23:15:14 +0300

    from_home_dz3 v3.0

--- lesson3.py
+++ lesson3.py
@@ -1,17 +1,17 @@
 # 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 
-# def abc():
-#     while True:
-#         try:
-#             a = int(input("a - "))
-#             b = int(input("b - "))
-#             c = a / b
-#             return c
-#         except ZeroDivisionError:
-#             print("Нельзя делить на ноль")
-#             break
-# print(abc())
+def abc():
+    while True:
+        try:
+            a = int(input("a - "))
+            b = int(input("b - "))
+            c = a / b
+            return c
+        except ZeroDivisionError:
+            print("Нельзя делить на ноль")
+            break
+print(abc())
 
 # 2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия,
 # год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы.
@@ -24,25 +24,25 @@
 #         user_input = input(f"{i.title()} - ")
 #         dict[i] = user_input
 #     return print(" ".join(dict.values()))
-#
-# def userdata2(**owner):
-#     return print(" ".join(owner.values()))
-#
-# userdata2(name = "Vladimir",
-#           surname = "Zelov",
-#           year = "10.11.1985",
-#           city = "Moscow",
-#           email = "abra@kadab.ra",
-#           phone = "9267776369")
+
+def userdata2(**owner):
+    return print(" ".join(owner.values()))
+
+userdata2(name = "Vladimir",
+          surname = "Zelov",
+          year = "10.11.1985",
+          city = "Moscow",
+          email = "abra@kadab.ra",
+          phone = "9267776369")
 
 
 # 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
 # и возвращает сумму наибольших двух аргументов.
 
-# def my_func(a, b, c):
-#     return print((a + b + c) - min(a, b, c))
-#
-# my_func(100,2,50)
+def my_func(a, b, c):
+    return print((a + b + c) - min(a, b, c))
+
+my_func(100,2,50)
 
 
 # 4. Программа принимает действительное положительное число x и целое отрицательное число y.
@@ -51,21 +51,21 @@
 # Подсказка: попробуйте решить задачу двумя способами. Первый — возведение в степень с помощью оператора **.
 # Второй — более сложная реализация без оператора **, предусматривающая использование цикла.
 
-# def my_func1(x, y):
-#     return print(x ** y)
-#
-# def my_func2(x, y):
-#     res = 1
-#     for i in range(abs(y)):
-#         res *= x
-#     if y >= 0:
-#         return print(res)
-#     else:
-#         return print(1 / res)
-#
-#
-# my_func1(3, -3)
-# my_func2(3, -3)
+def my_func1(x, y):
+    return print(x ** y)
+
+def my_func2(x, y):
+    res = 1
+    for i in range(abs(y)):
+        res *= x
+    if y >= 0:
+        return print(res)
+    else:
+        return print(1 / res)
+
+
+my_func1(3, -3)
+my_func2(3, -3)
 
 
 # 5. Программа запрашивает у пользователя строку чисел, разделенных пробелом.
@@ -74,49 +74,28 @@
 # вводится специальный символ, выполнение программы завершается. Если специальный символ введен после нескольких чисел,
 # то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
 
-# def my_func(*args):
-#     ls2 = []
-#     while True:
-#         if "*" in ls2:
-#             print(sum(ls2[:-1]))
-#             break
-#         user = list(input('Введите числа: ').split(" "))
-#         ls = []
-#         for i in user:
-#             if i == "*":
-#                 print("После символа * сумма не считается")
-#                 ls.append(i)
-#                 break
-#             else:
-#                 ls.append(int(i))
-#         ls2 += ls
-#         print(sum(ls2))
-#
-# my_func()
+def my_func():
+    total = 0
+    while True:
+        ls = input("Please enter the numbers. For exit press Q: ").split()
 
+        for i, el in enumerate(ls):
+            if el == "q" or el == "Q":
+                while i < len(ls):
+                    ls.pop(i)
+                return f"Sum = {sum(ls)}. Total sum = {sum(ls) + total}"
+            else:
+                try:
+                    ls[i] = int(el)
+                except ValueError:
+                    ls.remove(el)
+                    ls.insert(i, 0)
 
-# def sum_func():
-#     total_sum = 0
-#     while True:
-#         sum_list = input("Input any numbers separated by space to sum it. To exit enter 'Q': ").lower()
-#         sum_list = sum_list.split()
-#
-#         for i, item in enumerate(sum_list):
-#             if item == 'Q'.lower():
-#                 while i < len(sum_list):
-#                     sum_list.pop(i)
-#                 return f"New sum = {sum(sum_list)}. Total sum = {total_sum + sum(sum_list)}."
-#             else:
-#                 try:
-#                     sum_list[i] = int(item)
-#                 except ValueError:
-#                     sum_list.remove(item)
-#                     sum_list.insert(i, 0)
-#         print(f" New sum = {sum(sum_list)}. Total sum = {total_sum + sum(sum_list)}.")
-#         total_sum = total_sum + sum(sum_list)
-#
-#
-# print(sum_func())
+        print(f"Sum = {sum(ls)}. Total sum = {sum(ls) + total}")
+        total += sum(ls)
+
+print(my_func())
+
 
 # 6. Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же,
 # но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
--- test.py
+++
@@ -1,4 +0,0 @@
-def dog_conventer(name, age):
-    print(f"{name} {age * 7}")
-
-dog_conventer("Marta", 7)
