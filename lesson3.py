fcd99ad0e6d2bc1d5f86ca69e5d23e9d3e3be489
zil93rus <Zelovanastya2011>
2020-10-21 22:12:49 +0300

    from_work_dz3 v2.0

--- lesson3.py
+++ lesson3.py
@@ -95,28 +95,28 @@
 # my_func()
 
 
-def sum_func():
-    total_sum = 0
-    while True:
-        sum_list = input("Input any numbers separated by space to sum it. To exit enter 'Q': ").lower()
-        sum_list = sum_list.split()
-
-        for i, item in enumerate(sum_list):
-            if item == 'Q'.lower():
-                while i < len(sum_list):
-                    sum_list.pop(i)
-                return f"New sum = {sum(sum_list)}. Total sum = {total_sum + sum(sum_list)}."
-            else:
-                try:
-                    sum_list[i] = int(item)
-                except ValueError:
-                    sum_list.remove(item)
-                    sum_list.insert(i, 0)
-        print(f" New sum = {sum(sum_list)}. Total sum = {total_sum + sum(sum_list)}.")
-        total_sum = total_sum + sum(sum_list)
-
-
-print(sum_func())
+# def sum_func():
+#     total_sum = 0
+#     while True:
+#         sum_list = input("Input any numbers separated by space to sum it. To exit enter 'Q': ").lower()
+#         sum_list = sum_list.split()
+#
+#         for i, item in enumerate(sum_list):
+#             if item == 'Q'.lower():
+#                 while i < len(sum_list):
+#                     sum_list.pop(i)
+#                 return f"New sum = {sum(sum_list)}. Total sum = {total_sum + sum(sum_list)}."
+#             else:
+#                 try:
+#                     sum_list[i] = int(item)
+#                 except ValueError:
+#                     sum_list.remove(item)
+#                     sum_list.insert(i, 0)
+#         print(f" New sum = {sum(sum_list)}. Total sum = {total_sum + sum(sum_list)}.")
+#         total_sum = total_sum + sum(sum_list)
+#
+#
+# print(sum_func())
 
 # 6. Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же,
 # но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
@@ -124,5 +124,9 @@ print(sum_func())
 # Каждое слово состоит из латинских букв в нижнем регистре. Сделать вывод исходной строки, но каждое слово должно
 # начинаться с заглавной буквы. Необходимо использовать написанную ранее функцию int_func().
 
-def int_func():
-    pass
+def int_func(el):
+    return el.title()
+
+print(int_func("hello"))
+print(int_func("hello my friend"))
+
